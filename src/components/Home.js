bbfe385576e7c822572181fad6eede058cad0527
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-25 20:45:09 +0800

    Added beta warning

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,19 @@ function Home(){
 	fetch(raw).then(r=>r.text()).then(text=>setMainDescription(text))
 	return <div id="main-home-container">
 		<h1>The PeerRTC</h1>
-		<p id="home-description">{mainDescription}</p>
+		<p class="home-description">{mainDescription}</p>
 		<br/>
 		<center>	
 			{createGithubButtons("Fork PeerRTC", "https://github.com/PeerRTC/PeerRTC")}
 			{createGithubButtons("Fork AndroidPeerRTC", "https://github.com/PeerRTC/AndroidPeerRTC")}
 			{createGithubButtons("Fork PeerRTCServer", "https://github.com/PeerRTC/PeerRTC-Server")}
+		</center>
 
-			<br/><br/><br/><br/><br/>
+		<br/><br/>
+		<div class="home-description">This project is still on beta phase and can be unstable. Contributions and bug reports are all welcome.</div>
+
+		<br/>
+		<center>
 			<p id="home-attribution-req">
 				💡 Attribution to <a href={attribUrl} target="_blank">us</a> are required
 				 when using any modules and libraries from PeerRTC 💡
@@ -30,7 +35,6 @@ function Home(){
 
 			<button id="copy-attrib-bttn" onClick={copyAttrib}>Copy Attribution</button>
 			<div id="attrib-copied-indicator">Hooray, attribution copied !! <br/>{hooray}</div>
-
 		</center>
 		
 		
